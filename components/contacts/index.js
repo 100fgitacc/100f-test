8b093fde0d66ac2e492ecd90a80ff8916cb7126d
100fgitacc <100fsiteblog@gmail.com>
2024-09-30 13:02:28 +0300

    text fixes

--- components/contacts/index.js
+++ components/contacts/index.js
@@ -13,7 +13,7 @@ const Contacts = () => {
               <CoinsPlugin/>
               <div className={styles.inner}>
                   <h1 className="main-title">Have a crypto project or want to get early-stage access?<br/><span className={`title-span ${styles['contacts-span']}`}>Let&#39;s talk!</span></h1>
-                  <p className={styles.subtitle}>Whether you're interested in submitting your own project or participating in a crypto project early stages, we have a solution for you. Fill out the form, and let's talk!</p>
+                  <p className={styles.subtitle}>Whether you&apos;re interested in submitting your own project or participating in a crypto project early stages, we have a solution for you. Fill out the form, and let&apos;s talk!</p>
               </div>
           </div>
       </section>
