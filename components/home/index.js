59009c43ef82539f1f158c645cc040e2714ea169
100fgitacc <100fsiteblog@gmail.com>
2025-01-22 23:04:57 +0200

    fixed image bugs 12

--- components/home/index.js
+++ components/home/index.js
@@ -1058,19 +1058,19 @@ useEffect(() => {
               <div className={styles['sales-system']}>
                 <div className={styles['sales-system-item']}>
                   <h5 className={styles['sales-system-title']}>Auto webinar funnel</h5>
-                  {isMobile ? (<Image src='/assets/img/AutowebMiro.png' width={229} height={400} />) : (<Image src='/assets/img/Miro-m.png' width={229} height={285} />)}
-                  {/* <Image src='/assets/img/Bandle.png' width={229} height={167}  /> */}
+                  {/* {isMobile ? (<Image src='/assets/img/AutowebMiro.png' width={229} height={400} />) : (<Image src='/assets/img/Miro-m.png' width={229} height={285} />)} */}
+                  <Image src='/assets/img/Bandle.png' width={229} height={167}  />
                 </div>
                 <div className={styles['sales-system-item']}>
                   <h5 className={styles['sales-system-title']}>Curator-based funnel</h5>
                   {isMobile ? (<Image src='/assets/img/AutowebMiro1.png' width={229} height={285}  />) : (
                     <Image src='/assets/img/Miro-m2.png' width={229} height={285}  />
                   )}
-                  {/* <Image src='/assets/img/Bandle2.png' width={229} height={124}    /> */}
+                  <Image src='/assets/img/Bandle2.png' width={229} height={124}    />
                 </div>
                 <div className={styles['sales-system-item']}>
                   <h5 className={styles['sales-system-title']}>Video sales funnel</h5>
-                  {/* <Image 
+                  <Image 
                     src='/assets/img/AutowebMiro2.png' 
                     width={229} 
                     height={285} 
@@ -1081,7 +1081,7 @@ useEffect(() => {
                     width={229} 
                     height={124} 
                     sizes="(max-width: 768px) 100vw, 229px" 
-                  /> */}
+                  />
                 </div>
               </div>
               <div className={`${styles['paid-content']}`}>
