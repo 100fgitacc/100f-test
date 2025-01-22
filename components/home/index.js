85c67d0b0a62fa4565436713e6ed927e3127273b
100fgitacc <100fsiteblog@gmail.com>
2025-01-22 22:52:00 +0200

    fixed image bugs 4

--- components/home/index.js
+++ components/home/index.js
@@ -1055,7 +1055,7 @@ useEffect(() => {
             <div className={styles['card-section']}>
             
               <h2 className={styles['secondary-title']}>Cold Audience Lead Generation System</h2>
-              {/* <div className={styles['sales-system']}>
+              <div className={styles['sales-system']}>
                 <div className={styles['sales-system-item']}>
                   <h5 className={styles['sales-system-title']}>Auto webinar funnel</h5>
                   <Image src='/assets/img/AutowebMiro.png' width={229} height={400} />
@@ -1063,17 +1063,17 @@ useEffect(() => {
                 </div>
                 <div className={styles['sales-system-item']}>
                   <h5 className={styles['sales-system-title']}>Curator-based funnel</h5>
-                  <Image src='/assets/img/AutowebMiro1.png' width={229} height={285}  />
+                  {/* <Image src='/assets/img/AutowebMiro1.png' width={229} height={285}  /> */}
                   <Image src='/assets/img/Bandle2.png' width={229} height={124}    />
                 </div>
                 <div className={styles['sales-system-item']}>
                   <h5 className={styles['sales-system-title']}>Video sales funnel</h5>
-                  <Image 
+                  {/* <Image 
                     src='/assets/img/AutowebMiro2.png' 
                     width={229} 
                     height={285} 
                     sizes="(max-width: 768px) 100vw, 229px" 
-                  />
+                  /> */}
                   <Image 
                     src='/assets/img/Bandle3.png' 
                     width={229} 
@@ -1081,7 +1081,7 @@ useEffect(() => {
                     sizes="(max-width: 768px) 100vw, 229px" 
                   />
                 </div>
-              </div> */}
+              </div>
               <div className={`${styles['paid-content']}`}>
                 <h5 className={styles['paid-title']}>Paid Advertising</h5>
                 <div className={styles['paid-logos']}>
