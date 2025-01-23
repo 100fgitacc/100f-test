ee9c97c645d8ab192c088fff375caf9ef855484c
100fgitacc <100fsiteblog@gmail.com>
2025-01-23 14:19:53 +0200

    sendpulse API

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,51 +1,113 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const transporter = nodemailer.createTransport({
-      host: 'mail.adm.tools',
-      port: 465,
-      secure: true, 
-      auth: {
-        user: 'info@100f.com',
-        pass: 'pJT9X67ag9',
-      },
-    });
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Method not allowed" });
+    }
+
     const { name, email, company, whatsApp, capacity } = req.body;
-    console.log(req.body);
+
+    const clientId = process.env.SENDPULSE_CLIENT_ID;
+    const clientSecret = process.env.SENDPULSE_CLIENT_SECRET;
+   
+
+    async function getAccessToken() {
+        const response = await fetch("https://api.sendpulse.com/oauth/access_token", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                grant_type: "client_credentials",
+                client_id: clientId,
+                client_secret: clientSecret,
+            }),
+        });
+        const data = await response.json();
+        return data.access_token;
+    }
+
+    async function addSubscriber(token, formData, id) {
+        const response = await fetch(`https://api.sendpulse.com/addressbooks/${id}/emails`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: formData,
+        });
+
+        const data = await response.json();
     
-    let mailOptions;
-    if (name && email && capacity && whatsApp) {
-      mailOptions = {
-        from: '100Fcom_Pitch <info@100f.com>',
-        to: 'kir.ulanov@100f.com, admin@100f.com, info@100f.com',
-        subject: `100F.com Request Investment Deck`,
-        text: `
-          Name: ${name}
-          Email: ${email}
-          company: ${company}
-          WhatsApp: ${whatsApp}
-          Capacity: ${capacity}
-        `,
-      };
-    } else {
-      mailOptions = {
-        from: '100Fcom_EarlyAdopter <info@100f.com>',
-        to: 'kir.ulanov@100f.com, admin@100f.com, info@100f.com',
-        subject: `100F.com Request for Early Adoption`,
-        text: `
-          Name: ${name}
-          Email: ${email}
-        `,
-      };
+        if (!response.ok) {
+            throw new Error(data.error || 'Failed to add subscriber');
+        }
     }
+
     try {
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ success: true });
+        const token = await getAccessToken();
+        const transporter = nodemailer.createTransport({
+            host: "mail.adm.tools",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "info@100f.com",
+                pass: "pJT9X67ag9",
+            },
+        });
+
+        let mailOptions;
+        let sendpulseOptions;
+        const waitlistId = process.env.SENDPULSE_ADDRESS_BOOK_ID_WAITLIST;
+        const formId = process.env.SENDPULSE_ADDRESS_BOOK_ID_FORM;
+        if (name && email && capacity && whatsApp) {
+            mailOptions = {
+                from: "100Fcom_Pitch <info@100f.com>",
+                to: "kir.ulanov@100f.com, admin@100f.com, info@100f.com",
+                subject: `100F.com Request Investment Deck`,
+                text: `
+                    Name: ${name}
+                    Email: ${email}
+                    Company: ${company}
+                    WhatsApp: ${whatsApp}
+                    Capacity: ${capacity}
+                `,
+            };
+            sendpulseOptions = JSON.stringify({
+                emails: [{ 
+                    email, 
+                    variables: { 
+                        Имя: name, 
+                        Компания: company, 
+                        WhatsApp: whatsApp, 
+                        Емкость: capacity 
+                    } 
+                }],
+            }),
+            await addSubscriber(token,sendpulseOptions,formId);
+        } else {
+            mailOptions = {
+                from: "100Fcom_EarlyAdopter <info@100f.com>",
+                to: "kir.ulanov@100f.com, admin@100f.com, info@100f.com",
+                subject: `100F.com Request for Early Adoption`,
+                text: `
+                    Name: ${name}
+                    Email: ${email}
+                `,
+            };
+            sendpulseOptions = JSON.stringify({
+                emails: [{ 
+                    email, 
+                    variables: { 
+                        Имя: name
+                    } 
+                }],
+            }),
+            await addSubscriber(token,sendpulseOptions,waitlistId);
+        }
+
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ success: true });
     } catch (error) {
-      res.status(500).json({ success: false, error: error.message });
-    }
-    } else {
-      res.status(405).json({ message: 'Method not allowed' });
+        console.error("Error:", error);
+        res.status(500).json({ success: false, error: error.message });
     }
 }
