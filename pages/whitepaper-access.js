4ffea1e1f79a7635b1bc7f01795fb46f30bc877e
100fgitacc <100fsiteblog@gmail.com>
2025-01-31 16:43:08 +0200

    optimized images

--- pages/whitepaper-access.js
+++ pages/whitepaper-access.js
@@ -10,7 +10,7 @@ const WhitepaperAccessPage = () => {
   return(
     <>
     <Head>
-      <title>Submit a Project or Invest with 100F | Let&apos;s Talk!</title>
+      {/* <title>Submit a Project or Invest with 100F | Let&apos;s Talk!</title> */}
       <meta name="description" content="Are you interested in submitting a project or investing in one? Let's talk! We select projects with potential for market development and opportunities for joint returns. Complete the form to request a consultation with our crypto experts." />
       <meta name="keywords" content="crypto projects, investing, submit a project, market development, joint returns, crypto consultation, 100F" />
       <meta name="author" content="100F" />
